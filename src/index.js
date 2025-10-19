e04bb2cca2863d9f39d700ecd4bf73ee661f7d44
RimaGai <puolus@gmail.com>
2025-10-19 11:30:24 +0200

    Show real time and date

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  let parisDate = document.querySelector("#paris .date");
+  let parisTime = document.querySelector("#paris .time");
+  let parisNow = moment().tz("Europe/Paris");
+  parisDate.innerHTML = parisNow.format("dddd, MMMM D, YYYY");
+  parisTime.innerHTML = parisNow.format("HH:mm:ss A");
 
+  let sydneyDate = document.querySelector("#sydney .date");
+  let sydneyTime = document.querySelector("#sydney .time");
+  let sydneyNow = moment().tz("Australia/Sydney");
+  sydneyDate.innerHTML = sydneyNow.format("dddd, MMMM D, YYYY");
+  sydneyTime.innerHTML = sydneyNow.format("HH:mm:ss A");
+
+  let tokyoDate = document.querySelector("#tokyo .date");
+  let tokyoTime = document.querySelector("#tokyo .time");
+  let tokyoNow = moment().tz("Asia/Tokyo");
+  tokyoDate.innerHTML = tokyoNow.format("dddd, MMMM D, YYYY");
+  tokyoTime.innerHTML = tokyoNow.format("HH:mm:ss A");
+}
+
+updateTime();
+
+setInterval(updateTime, 1000);
